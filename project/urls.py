7ae0befb5dd0bdf463b9f60153229fdb744c1a5d
diego.policarpo <policarpodamae@gmail.com>
2023-11-26 20:59:57 -0300

    [TESTE] testando namespace e templates

--- project/urls.py
+++ project/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('recipes.urls')), # significa que inicia pela barra / ex: 'dominio/'
+    #path('recipes/', include('recipes.urls')) # significa que inicia pelo recipes/ ex:'dominio/recipes/'
 ]
+
---
+++ recipes/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from datetime import datetime, timedelta, timezone
+
+
+def horas():
+    fuso = timezone(timedelta(hours=-3))
+    hora_br = datetime.now(fuso)
+    if 0 == hora_br.hour < 12:
+        return 'Bom dia'
+    elif 12 > hora_br.hour < 18:
+        return 'Boa tarde'
+    else:
+        return 'Boa noite' 
+
+def home(request):    
+    return render(request, 'recipes/home.html', context= {
+        'name': 'Diego!',
+        'periodo': horas()
+    })
+
+def contato(request):
+    return render(request, 'me-apague/temp.html')
+
+def sobre(request):
+    return HttpResponse('SOBRE')
